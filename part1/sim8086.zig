da0749300eebc0bc3b463ec0a5c2a84a82ae9107
Ben Brinckerhoff <ben@bbrinck.com>
2023-09-23 17:41:04 -0600

    Parsed the parts of the bytes

--- part1/sim8086.zig
+++ part1/sim8086.zig
@@ -24,5 +24,30 @@ fn decode(filename: []const u8) !void {
     var file = try fs.cwd().openFile(filename, .{});
     defer file.close();
 
-    try stdout.print("  {s}", .{filename});
+    var buffer: [100]u8 = undefined;
+    try file.seekTo(0);
+    _ = try file.readAll(&buffer);
+
+    // Manual is https://edge.edx.org/c4x/BITSPilani/EEE231/asset/8086_family_Users_Manual_1_.pdf (page 164)
+    // 6 bits are instruction code
+    const inst_code = (buffer[0] & 0b11111100) >> 2;
+
+    // 2 bits are "DW" (one bit each)
+    const dw = (buffer[0] & 0b00000011);
+
+    // 2 bits are "mod"
+    // 3 bits are "reg"
+    // 3 bits are r/m
+    const mod = (buffer[1] & 0b11000000) >> 6;
+    _ = mod; // we know mod is always 11
+
+    const reg = (buffer[1] & 0b00111000) >> 3;
+    const r_m = (buffer[1] & 0b00000111);
+
+    const instruction = switch (inst_code) {
+        0b100010 => "mov",
+        else => "unknown",
+    };
+
+    try stdout.print("  {s} {s} {b} {b}", .{ filename, instruction, inst_code, dw });
 }
